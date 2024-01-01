82e88fea78cd762f31da44fb9abc1cd2ae30df2c
Ashwin Verma <111328257+Ashwin835@users.noreply.github.com>
2024-01-01 12:17:45 -0500

    Update 217: Contains_Duplicate.py

--- 217: Contains_Duplicate.py
+++ 217: Contains_Duplicate.py
@@ -1,9 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        visited=set()
-        for i in nums:
-            if i in visited:
+        visited= set()
+        for cur in nums:
+            if cur in visited: #O(1) hashing lookup time
                 return True
-            visited.add(i)
-        return False
-        #runs in O(n) time
+            else:
+                visited.add(cur)  
+        return False 
