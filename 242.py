77861732d7bf9720dc70dfdd94e37fd6a0ee89b8
Ashwin Verma <111328257+Ashwin835@users.noreply.github.com>
2024-01-02 16:44:38 -0500

    Update 242.py

--- 242.py
+++ 242.py
@@ -1,6 +1,10 @@
 import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         return collections.Counter(s)==collections.Counter(t)
-        #2n times will it add elements to the hash-table, and comparing will be 2n times
-        # each operation takes O(1)
+        # n= length of s and t
+        #2n times will it add elements to the hash-tables, and comparing will be n times
+        # each add and fetch operation takes O(1)
+        #Big O: O(n)
